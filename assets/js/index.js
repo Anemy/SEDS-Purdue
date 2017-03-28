2397b14ef52b6c699090cf88c6a8f8ed87e433c5
Rhys Howell <rhysh@live.com>
2017-03-28 01:16:58 -0400

    Added slowmo video news

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
-console.log('Solar system by Malik Dellidj: http://codepen.io/kowlor/pen/ZYYQoy');
+const alienString = `
+       .-""""-.        .-""""-.
+      /        \\      /        \\
+     /_        _\\    /_        _\\
+    // \\      / \\\\  // \\      / \\\\
+    |\\__\\    /__/|  |\\__\\    /__/|
+     \\    ||    /    \\    ||    /
+      \\        /      \\        /
+       \\  __  /        \\  __  / 
+        '.__.'          '.__.'
+         |  |            |  |
+         |  |            |  |
+`;
+
+console.log(alienString);
+console.log('✌️ We come in peace ✌️ Thanks for checking out Purdue SEDS!');
+console.log('You can look at the code powering this site at: https://github.com/Anemy/SEDS-Purdue');
